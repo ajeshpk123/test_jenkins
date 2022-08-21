df1f7ebad6974f74e54934e4ac04146eb12ef8e5
ajeshpk123 <107336052+ajeshpk123@users.noreply.github.com>
2022-08-21 12:19:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,11 @@
 #! /bin/bash
 
-whoami ;
-pwd ;
-echo "hello-world" 
-/usr/bin/kubectl
-echo "complete-ok" 
-                sh " sudo -i "
-                sh " echo  Start-K8s-Pod-deploymnet "
-                sh "/usr/bin/kubectl apply -f  pod.yaml  -n ajesh2"
-                sh "kubectl get pods -o wide -n ajesh2"
-                sh "kubectl apply -f service.yaml -n ajesh2"
-                sh "Kubectl get svc  -n ajesh2" 
+whoami 
+sudo -i 
+whoami
+echo  Start-K8s-Pod-deploymnet 
+kubectl apply -f  pod.yaml  -n ajesh2
+kubectl get pods -o wide -n ajesh2
+kubectl apply -f service.yaml -n ajesh2
+Kubectl get svc  -n ajesh2
 
