c4d7dbd2419310a86bd4399d55b85082dd0beae0
ajeshpk123 <107336052+ajeshpk123@users.noreply.github.com>
2022-08-21 12:30:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@ whoami
 echo  Start-K8s-Pod-deploymnet 
 kubectl apply -f  pod.yaml  -n ajesh2
 kubectl get pods -o wide -n ajesh2
-kubectl apply -f service.yaml -n ajesh2
-Kubectl get svc  -n ajesh2
+kubectl apply -f service.yaml -n ajesh2 ;
+Kubectl get svc  -n ajesh2 ;
+echo "deploy-suess"
 
